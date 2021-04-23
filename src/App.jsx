5bec334eb1f8592cc22b39a9dd78177e4739aa23
Camilo Sanchez <daniel.sanchez24102001@gmail.com>
2021-04-22 21:12:50 -0500

    adding the new variant with animations

--- src/App.jsx
+++ src/App.jsx
@@ -21,15 +21,13 @@ function App() {
 	//Fetch Tasks
 	const fetchTasks = async () => {
 		const res = await fetch(`http://localhost:5000/tasks`);
-		const data = await res.json();
-		return data;
+		return await res.json();
 	};
 
 	//Fetch Task
 	const fetchTask = async (id) => {
 		const res = await fetch(`http://localhost:5000/tasks/${id}`);
-		const data = await res.json();
-		return data;
+		return await res.json();
 	};
 
 	// Delete Task
